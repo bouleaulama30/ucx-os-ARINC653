82a1465afa661d315336bb97e15facc1b49f1c3d
bouleaulama30 <146018011+bouleaulama30@users.noreply.github.com>
2026-07-09 19:39:07 -0400

    change routing table size by something dynamically configurable

--- arinc/static/static_conf_testperf.c
+++ arinc/static/static_conf_testperf.c
@@ -2,7 +2,6 @@
 
 const int routing_table_size = sizeof(system_port_table) / sizeof(system_port_table[0]);
 
-
 const ERROR_ACTION_TYPE hm_table_partition_1[4][4] = {
     // Colonnes : DEADLINE_MISSED, APPLICATION_ERROR, NUMERIC_ERROR, DEFAULT
     
@@ -51,21 +50,31 @@ const ERROR_ACTION_TYPE hm_table_module[1][4] = {
     { PROCESS_REPLENISH, PROCESS_STOP, PROCESS_RESTART, PROCESS_STOP }
 };
 
+#ifndef MEASURE_STATIC
 __attribute__((section(".p1_code")))
 void p1_main_process(struct pcb_s *partition){
     RETURN_CODE_TYPE return_code0;
     RETURN_CODE_TYPE return_code1;
     PROCESS_ID_TYPE process_id_0;
     PROCESS_ID_TYPE process_id_1;
+    PROCESS_ID_TYPE process_id_2;
 
     CREATE_PROCESS(&P1_PROCESS_1_CONFIG, &process_id_0, &return_code0);
     printf("CREATE PROCESS %d and Error code is %d\n", process_id_0, return_code0);      
     CREATE_PROCESS(&P1_PROCESS_2_CONFIG, &process_id_1, &return_code0);
     printf("CREATE PROCESS %d and Error code is %d\n", process_id_1, return_code0);      
+    CREATE_PROCESS(&P1_PROCESS_3_CONFIG, &process_id_2, &return_code0);
+    printf("CREATE PROCESS %d and Error code is %d\n", process_id_2, return_code0);      
+
+    CREATE_ERROR_HANDLER(DEFAULT_ERROR_HANDLER_CONFIG.entry_point, DEFAULT_ERROR_HANDLER_CONFIG.stack_size, &return_code1);
+
+    printf("CREATE ERROR HANDLER PROCESS Error code is %d\n", return_code1);
 
     START(process_id_0, &return_code0);  
     START(process_id_1, &return_code1);  
-    
+
+    START(process_id_2, &return_code0);  
+
     SET_PARTITION_MODE(NORMAL, &return_code0);
 }
 
@@ -73,6 +82,7 @@ __attribute__((section(".p2_code")))
 void p2_main_process(struct pcb_s *partition){
     RETURN_CODE_TYPE return_code0;
     RETURN_CODE_TYPE return_code1;
+    PROCESS_ID_TYPE process_id_0;
     PROCESS_ID_TYPE process_id_1;
 
     CREATE_PROCESS(&P2_PROCESS_1_CONFIG, &process_id_1, &return_code1);
@@ -83,3 +93,4 @@ void p2_main_process(struct pcb_s *partition){
     
     SET_PARTITION_MODE(NORMAL, &return_code0);
 }
+#endif
